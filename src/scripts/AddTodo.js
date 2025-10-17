59bf864499e9d3f32328603a1def5007b415e8e0
Aditya Difa Amanullah <adityadifa123@gmail.com>
2025-10-17 16:25:29 +0700

    final version 1

--- src/scripts/AddTodo.js
+++ src/scripts/AddTodo.js
@@ -1,17 +1,56 @@
 export default function AddTodo(e, textTag, parent) {
   e.preventDefault();
+  let status = "on progress";
+
   const parentDiv = document.getElementById(parent);
   const textValue = document.getElementById(textTag).value;
 
   const cardDiv = document.createElement("div");
-  cardDiv.classList = "p-2 bg-white shadow-md/10 shadow-[#30332E] my-2";
+  cardDiv.classList =
+    "w-max-full p-2 bg-white shadow-md/10 shadow-[#30332E] my-2 flex";
 
-  const fragment = document.createDocumentFragment();
+  const divText = document.createElement("div");
+  divText.classList = "basis-4/5";
+
+  const divStatus = document.createElement("div");
+  divStatus.classList = "basis-1/5 flex flex-col justify-center";
 
   const textDescription = document.createElement("p");
-  textDescription.textContent = textValue;
+  textDescription.innerHTML = textValue;
+  textDescription.classList = "break-all";
 
-  fragment.appendChild(textDescription);
-  cardDiv.appendChild(fragment);
+  const buttonStatus = document.createElement("button");
+  buttonStatus.classList =
+    "p-1 text-white font-bold rounded-md bg-yellow-400 m-1";
+  buttonStatus.type = "button";
+  buttonStatus.textContent = status;
+  buttonStatus.addEventListener("click", () => {
+    if (status === "on progress") {
+      status = "done";
+      buttonStatus.classList =
+        "p-1 text-white font-bold rounded-md bg-green-400 m-1";
+    } else if (status === "done") {
+      status = "on progress";
+      buttonStatus.classList =
+        "p-1 text-white font-bold rounded-md bg-yellow-400 m-1";
+    }
+    buttonStatus.textContent = status;
+  });
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList = "p-1 text-white font-bold rounded-md bg-red-400 m-1";
+  buttonDelete.type = "button";
+  buttonDelete.textContent = "Delete";
+  buttonDelete.addEventListener("click", () => {
+    cardDiv.remove();
+  });
+
+  divText.appendChild(textDescription);
+  divStatus.appendChild(buttonStatus);
+  divStatus.appendChild(buttonDelete);
+
+  cardDiv.appendChild(divText);
+  cardDiv.appendChild(divStatus);
+
   parentDiv.appendChild(cardDiv);
 }
